c8a1937b61e0e68f7e630e3d619cba4f0b70183d
Nick Amin <namin@umail.ucsb.edu>
2015-06-20 22:12:11 -0700

    update gitignore and use different file

--- comparisons/readme.txt
+++ comparisons/readme.txt
@@ -1,3 +1,6 @@
 update run.C with desired file. then
-$ root -b -q compare.C
+$ root -b -q run.C
 will output specified pdf files comparing branches in a tree.
+
+
+don't forget to do "cmsenv"!
--- comparisons/run.C
+++ comparisons/run.C
@@ -2,12 +2,6 @@
   gROOT->ProcessLine(".L dataMCplotMaker.cc+");
   gROOT->ProcessLine(".L compare.C+");
   TChain *ch = new TChain("Events");
-  // ch->Add("/hadoop/cms/store/user/namin/MinimumBias/crab_metjob2_MinBias2015A/150620_043825/0000/*.root");
-  ch->Add("/hadoop/cms/store/user/namin/MinimumBias/crab_metjob2_MinBias2015A/150620_043825/0000/ntuple_*.root");
-  // ch->Add("/hadoop/cms/store/user/namin/MinimumBias/crab_metjob2_MinBias2015A/150620_043825/0000/ntuple_100.root");
-  // gROOT->ProcessLine("compare(\"/home/users/namin/2015/met/CMSSW_7_4_4_patch1/src/DataSleuth/DataSleuth/test/ntuple.root\")");
-  // gROOT->ProcessLine("compare(\"/hadoop/cms/store/user/namin/MinimumBias/crab_metjob2_MinBias2015A/150620_043825/0000/ntuple_9.root\")");
-
-
+  ch->Add("/hadoop/cms/store/user/namin/MinimumBias/crab_metjob2_MinBias2015A/150620_043825/0000/ntuple_100.root");
   compare(ch);
 }
