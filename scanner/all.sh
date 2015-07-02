ac42a4e510a579246c5f5e01edece097c52fada5
Nick Amin <namin@umail.ucsb.edu>
2015-07-02 09:51:23 -0700

    add all.sh

---
+++ scanner/all.sh
@@ -0,0 +1,5 @@
+rm -f pdfs/*.pdf
+root -b -q doAll.C
+gs -dBATCH -dNOPAUSE -q -sDEVICE=pdfwrite -sOutputFile=merged.pdf pdfs/*.pdf
+# cp merged.pdf ~/public_html/dump/
+scp merged.pdf squark.physics.ucsb.edu:~/Desktop/
